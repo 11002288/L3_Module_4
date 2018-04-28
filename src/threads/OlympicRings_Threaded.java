3c1dcdb14d261bb153fe6007b28c8063d6baa0fa
RationalGeographic <league@WTS20.attlocal.net>
2018-04-28 10:37:56 -0700

    done
    done

--- src/threads/OlympicRings_Threaded.java
+++ src/threads/OlympicRings_Threaded.java
@@ -11,15 +11,15 @@ public class OlympicRings_Threaded {
 			Robot a = new Robot(700, 700);
 			Robot b = new Robot(850, 500);
 			Robot c = new Robot(1000, 700);
-			Robot d = new Robot(1200, 500);
-			Robot e = new Robot(500, 500);
+			Robot d = new Robot(1100, 500);
+			Robot e = new Robot(600, 500);
 			
 			a.setSpeed(10);
 			b.setSpeed(10);
 			c.setSpeed(10);
 			d.setSpeed(10);
 			e.setSpeed(10);
-			for (int i = 0; i < 180; i++) {
+			/*&for (int i = 0; i < 180; i++) {
 			a.turn(2);
 			a.move(5);
 			}
@@ -39,18 +39,37 @@ public class OlympicRings_Threaded {
 			e.turn(2);
 			e.move(5);
 			}
+			*/
+			a.penDown();
+			b.penDown();
+			c.penDown();
+			d.penDown();
+			e.penDown();
+			a.setPenColor(Color.YELLOW);
+			c.setPenColor(Color.GREEN);
+			d.setPenColor(Color.red);
+			e.setPenColor(Color.BLUE);
+			a.setPenWidth(200);
+			b.setPenWidth(200);
+			c.setPenWidth(200);
+			d.setPenWidth(200);
+			e.setPenWidth(200);
+		   
+			for (int i = 0; i < 180; i++) {
+				
 			
-		    Thread r1 = new Thread(()->a.move(400));
-			Thread r2 = new Thread(()->b.move(400));
-			Thread r3 = new Thread(()->c.move(400));
-			Thread r4 = new Thread(()->d.move(400));
-			Thread r5 = new Thread(()->e.move(400));
+			Thread r1 = new Thread(()->a.move(5));a.turn(2);
+			Thread r2 = new Thread(()->b.move(5));b.turn(2);
+			Thread r3 = new Thread(()->c.move(5));c.turn(2);
+			Thread r4 = new Thread(()->d.move(5));d.turn(2);
+			Thread r5 = new Thread(()->e.move(5));e.turn(2);
 			
 			r1.start();
 			r2.start();
 			r3.start();
 			r4.start();
 			r5.start(); 
+			}
 		}
 }
 
